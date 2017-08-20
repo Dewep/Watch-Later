57484c4053b84e12ab70aea9e4915036a137c8b7
Maigret Aurélien <dewep.net@gmail.com>
2017-08-20 11:39:33 +0200

    [TMDb] Add TMDb.discover endPoint

--- tmdb/index.js
+++ tmdb/index.js
@@ -1,10 +1,42 @@
+const request = require('request-promise')
+
 class TMDb {
   constructor (config, app) {
     this.config = config
     this.app = app
   }
 
-  run () {
+  async run () {
+    // this.app.tasks.runTask('update-news').catch(() => {})
+  }
+
+  async request (endPoint, qs, body, method) {
+    if (!method) {
+      method = 'GET'
+      body = '{}'
+    }
+    if (this.config.apiKey) {
+      qs.api_key = this.config.apiKey
+    }
+    var options = {
+      method,
+      url: `https://api.themoviedb.org/3${endPoint}`,
+      qs,
+      body
+    }
+    try {
+      return await request(options)
+    } catch (err) {
+      const simplifiedError = new Error(err.error || err.message)
+      simplifiedError.name = err.name
+      simplifiedError.statusCode = err.statusCode
+      throw simplifiedError
+    }
+  }
+
+  async discover (qs) {
+    const response = await this.request('/discover/movie', qs)
+    return response.results
   }
 }
 
