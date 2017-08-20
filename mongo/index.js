92daee360e856fbbc0d36aa608df8dbe1812c090
Maigret Aurélien <dewep.net@gmail.com>
2017-08-20 11:38:38 +0200

    [Mongo] Add database connection

--- mongo/index.js
+++ mongo/index.js
@@ -1,10 +1,36 @@
+const mongodb = require('mongodb')
+
 class Mongo {
   constructor (config, app) {
     this.config = config
     this.app = app
+    this.dbPromise = null
   }
 
-  run () {
+  async run () {
+    return await this._instance()
+  }
+
+  formatID (id) {
+    try {
+      return mongodb.ObjectID(id)
+    } catch (err) {
+      throw new Error('Identifier not found')
+    }
+  }
+
+  async _instance (collectionName) {
+    if (!this.dbPromise) {
+      const url = `mongodb://${this.config.host}:${this.config.port}/${this.config.database}`
+      this.dbPromise = mongodb.MongoClient.connect(url, { reconnectTries: 5 })
+    }
+
+    if (!collectionName) {
+      return await this.dbPromise
+    }
+
+    const db = await this.dbPromise
+    return await db.collection(collectionName)
   }
 }
 
