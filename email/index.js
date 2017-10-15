0f5110366e92914fc6eca10728d519f71c10a296
Maigret AurÃ©lien <dewep.net@gmail.com>
2017-10-15 16:07:11 +0200

    [Server] Add recover-password + improve new-user task

--- email/index.js
+++ email/index.js
@@ -45,6 +45,11 @@ class Email {
     const html = await this.app.template.render('email/updates', { name, movies })
     await this.sendEmail(email, name, 'New movies available on your Watch-Later', html)
   }
+
+  async sendRecoverPassword (email, name, token, newAccount) {
+    const html = await this.app.template.render('email/password', { name, token, newAccount })
+    await this.sendEmail(email, name, newAccount ? 'New Watch-Later account' : 'Reset your Watch-Later password', html)
+  }
 }
 
 module.exports = Email
